8dbed27ab389957332ad206ed1bc632b7387a2fe
JaymezS <james.shaojiaxuan@gmail.com>
2024-08-01 20:18:44 -0400

    Added Driver File

---
+++ src/Driver.py
@@ -0,0 +1,7 @@
+from LinearStockPredictionModel import LongTermStockModelVersion1
+from LinearStockPredictionModelTrainer import ModelTrainer
+from TrainingDataRequester import TrainingDataRequester
+
+class Driver:
+  def __init__(self):
+    pass
