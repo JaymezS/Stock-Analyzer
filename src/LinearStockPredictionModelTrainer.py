c18362d4e9e875a7929476f252033725b89c9465
JaymezS <james.shaojiaxuan@gmail.com>
2024-08-01 20:18:17 -0400

    Created Model Trainer

---
+++ src/LinearStockPredictionModelTrainer.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import numpy
+
+class ModelTrainer():
+  def __init__(self, model: nn.Module):
+    self.model = model
+    self.epochs = 1000
+    self.criterion = nn.CrossEntropyLoss()
+    self.optimizer = torch.optim.Adam(self.model.parameters(), lr = 0.001)
+    self.losses = []
+
+
+  def train(self, train_x, train_y):
+    for i in range(self.epochs):
+      y_pred = self.model.forward(train_x)
+      loss = self.criterion(y_pred, train_y)
+      self.losses.append(loss.detach().numpy())
+      self.optimizer.zero_grad()
+      loss.backward()
+      self.optimizer.step()
